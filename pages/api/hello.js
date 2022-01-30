bbf6c89623688e0a2c2e427d5e30ffd53ba84bcd
Alex Rusin <alex@sunriseintegration.com>
2022-01-30 14:21:43 -0800

    add winston logger

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import logger from '../../services/logger';
+
 export default function handler(req, res) {
+  logger.info(`Request headers: ${JSON.stringify(req.headers)}`);
+
   res.status(200).json({ name: 'John Doe' });
 }
